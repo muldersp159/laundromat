9d956040e4ad18e4fb28e0bed8bcff04d635f077
muldersp159 <20muldersp@marash.qld.edu.au>
2020-05-06 03:26:51 +0000

    automated movement

--- brickpiflask1/static/scripts/missioncontrol.js
+++ brickpiflask1/static/scripts/missioncontrol.js
@@ -1,12 +1,27 @@
-var shutdown = false;
+//var shutdown = false;
 var recurringhandle = null;  //can be used to delete recurring function if you want
-recurringhandle = setInterval(get_current_command, 1000);
+
+//create a new pen object attached to our canvas tag
+var turtle = new Pen("mycanvas");
+turtle.canvas.translate(.5, .5);
+turtle.pensize(3);
+turtle.penstyle("#000");
+turtle.pendown();
+turtle.jump(200,200);
+
 
 function shutdownserver(){
     clearInterval(recurringhandle);
     setTimeout(() => { console.log("Shutting down"); }, 1000);
     JSONrequest('/shutdown','POST');
     shutdown = true;
+}
+
+funciton draw_path(result)
+{
+    console.log(result);
+    turtle.angle(result.heading);
+    turtle.go(result.elapsedtime*20).stroke();
 }
 
 //THis recurring function gets data using JSON
@@ -21,3 +36,4 @@ function get_current_command() {
 function writecurrentcommand(results) {
     document.getElementById('message').innerHTML = results.currentcommand;
 }
+
