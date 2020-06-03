d136e3e90a0d045a9835740d68ebb1ffdba9b913
muldersp159 <20muldersp@marash.qld.edu.au>
2020-06-03 15:06:36 +1000

    drawing junctions too

--- brickpiflask1/static/scripts/map.js
+++ brickpiflask1/static/scripts/map.js
@@ -38,7 +38,7 @@ function go_through_events(results){
             else if (eventtype == "Fire Detected"){
                 turtle.penup();
                 turtle.angle(heading);
-                turtle.go(5);
+                turtle.go(8);
                 turtle.set();
                 turtle.begin();
                 turtle.home();
@@ -65,7 +65,7 @@ function go_through_events(results){
             else if (eventtype == "Victim Found"){
                 turtle.penup();
                 turtle.angle(heading);
-                turtle.go(5);
+                turtle.go(8);
                 turtle.set();
                 turtle.begin();
                 turtle.home();
@@ -87,6 +87,32 @@ function go_through_events(results){
                 turtle.go(5);
                 turtle.angle(heading);
                 turtle.pendown();
+            }
+            
+            else if (eventtype == "Junction Detected"){
+                turtle.penup();
+                turtle.angle(heading);
+                turtle.set();
+                turtle.begin();
+                turtle.home();
+                turtle.turn(180);
+                turtle.go(4);
+                turtle.pensize(0);
+                turtle.pendown();
+                turtle.fillstyle("#ff0000");
+                turtle.turn(90)
+                turtle.go(3).fill();
+                var t = 1
+                while (t < 4){
+                    turtle.turn(90);
+                    turtle.go(6).fill();
+                    t = t + 1
+                }
+                turtle.turn(90);
+                turtle.go(3).fill();
+                turtle.turn(-90);
+                turtle.angle(heading);
+                console.log("ollah");
             }
             var i = i + 3;
             var h = h + 3;
