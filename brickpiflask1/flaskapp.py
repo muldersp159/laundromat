57f1895e5c5ee5f93f4ffb0bc8b3be96285e2c61
muldersp159 <57078174+muldersp159@users.noreply.github.com>
2021-01-06 12:01:47 +1000

    culling

--- brickpiflask1/flaskapp.py
+++ brickpiflask1/flaskapp.py
@@ -5,7 +5,7 @@ from interfaces.databaseinterface import DatabaseHelper
 from datetime import datetime
 
 #Create a way to interact with database
-database = DatabaseHelper('test.sqlite')
+database = DatabaseHelper('laundromat.sqlite')
 
 #Global Variables
 app = Flask(__name__)
@@ -42,167 +42,22 @@ def missioncontrol():
     if 'userid' not in session:
         return redirect('./') #no form data is carried across using 'dot/'
     results = None
-    return render_template("missioncontrol.html", configured = robot.Configured, voltage = robot.get_battery())
+    return render_template("missioncontrol.html")
 
 #dashboard
 @app.route('/sensorview', methods=['GET','POST'])
 def sensorview():
-    if not robot.Configured: #make sure robot is funcitoning
-        return redirect('./')
     if 'userid' not in session:
         return redirect('./')
-    return render_template("sensorview.html", configured = robot.Configured)
-
-#get all stats and return through JSON
-@app.route('/getallstats', methods=['GET','POST'])
-def getallstats():
-    if 'userid' not in session:
-        return redirect('./') #no form data is carried across using 'dot/'
-    robot.CurrentCommand = "getting all stats"
-    results = robot.get_all_sensors()
-    return jsonify(results)
+    return render_template("sensorview.html")
 
 #map or table of fire and path data
 @app.route('/map')
 def map():
-    if not robot.Configured: #make sure robot is
-        return redirect('./')
     if 'userid' not in session:
         return redirect('./') #no form data is carried across using 'dot/'
     results = None
-    return render_template('map.html', results=results, configured = robot.Configured)
-
-#start robot moving
-@app.route('/start', methods=['GET','POST'])
-def start():
-    if not robot.Configured: #make sure robot is
-        return jsonify({ "message":"robot not yet configured"})
-    robot.CurrentCommand = "starting"
-    duration = None
-    duration = robot.move_power_untildistanceto(RPOWER, LPOWER, 20)
-    return jsonify({ "message":"starting", "duration":duration }) #jsonify take any type and makes a JSON
-    
-#start robot moving backwards
-@app.route('/reverse', methods=['GET','POST'])
-def reverse():
-    if not robot.Configured: #make sure robot is
-        return jsonify({ "message":"robot not yet configured"})
-    robot.CurrentCommand = "reversing"
-    robot.rotate_power_degrees_IMU(20, 180)
-    #save data to the databas
-    return jsonify({ "message":"reversing", "duration":duration }) #jsonify take any type and makes a JSON
-
-@app.route('/turnright', methods=['GET','POST'])
-def turnright():
-    if not robot.Configured: #make sure robot is
-        return jsonify({ "message":"robot not yet configured"})
-    robot.CurrentCommand = "turning right"
-    robot.rotate_power_degrees_IMU(20, 90)
-    #save data to the databas
-    return jsonify({ "message":"turning right", "duration":duration }) #jsonify take any type and makes a JSON
-
-@app.route('/turnleft', methods=['GET','POST'])
-def turnleft():
-    if not robot.Configured: #make sure robot is
-        return jsonify({ "message":"robot not yet configured"})
-    robot.CurrentCommand = "turning left"
-    robot.rotate_power_degrees_IMU(20, -90)
-    #save data to the databas
-    return jsonify({ "message":"turning left", "duration":duration }) #jsonify take any type and makes a JSON
-
-@app.route('/closeclaw', methods=['GET','POST'])
-def closeclaw():
-    if not robot.Configured: #make sure robot is
-        return jsonify({ "message":"robot not yet configured"})
-    robot.CurrentCommand = "closing claw"
-    while robot.CurrentCommand != "stop":
-        duration = robot.close_claw()
-    #save data to the databas
-    return jsonify({ "message":"closing claw", "duration":duration }) #jsonify take any type and makes a JSON
-
-@app.route('/openclaw', methods=['GET','POST'])
-def openclaw():
-    if not robot.Configured: #make sure robot is
-        return jsonify({ "message":"robot not yet configured"})
-    robot.CurrentCommand = "opening claw"
-    while robot.CurrentCommand != "stop":
-        duration = robot.open_claw()
-    #save data to the databas
-    return jsonify({ "message":"opening claw", "duration":duration }) #jsonify take any type and makes a JSON
-
-@app.route('/tojunction', methods=['GET','POST'])
-def movetojunction():
-    if not robot.Configured: #make sure robot is configured
-        return jsonify({ "message":"robot not yet configured"})
-    robot.CurrentCommand = "moving until junction"
-    duration = None
-    duration = robot.move_power_untiljunction(RPOWER, LPOWER, 20, junctionColour)
-    jsonify({ "message":"moving until junction", "duration":duration }) #jsonify take any type and makes a JSON
-    return navigatejunction()
-
-def navigatejunction():
-    if robot.CurrentCommand != "stop":
-        robot.CurrentCommand = "navigating junction"
-        distancemeasured = robot.get_ultra_sensor()
-        if distancemeasured >= 20 and distancemeasured != 0.0:
-            navigateintersection()
-        else:   
-            tempmeasured = robot.get_thermal_sensor()
-            if tempmeasured > 40:
-                reverse()
-                movetojunction()
-                #a fire was detected, so the robot reversed
-                return jsonify({ "message":"fire detected"})
-            elif tempmeasured < 10:
-                #using an icepack or cold can of soft drint for victim
-                return jsonify({ "message":"victim found"})
-            else:
-                navigatewall()
-    return jsonify({ "message":"navigating junction"})
-
-def navigateintersection():
-    robot.CurrentCommand = "navigating intersection"
-    robot.rotate_power_degrees_IMU(20, -90)
-    distancemeasured = robot.get_ultra_sensor() #reading ultrasonic to see if there is a wall infront
-    if distancemeasured >= 20 and distancemeasured != 0.0:
-        movetojunction()
-        #turned left
-    else:
-        robot.rotate_power_degrees_IMU(20, 90)
-        distancemeasured = robot.get_ultra_sensor()
-        if distancemeasured >= 20 and distancemeasured != 0.0:
-            movetojunction()
-            #went straight
-        else:
-            robot.rotate_power_degrees_IMU(20, 90)
-            distancemeasured = robot.get_ultra_sensor()
-            if distancemeasured >= 20 and distancemeasured != 0.0:
-                movetojunction()
-                #turned right
-            else:
-                robot.rotate_power_degrees_IMU(20, 90)
-                movetojunction()
-                #reversed
-    return jsonify({ "message":"navigating intersection"})
-
-def navigatewall():
-    robot.CurrentCommand = "navigating wall"
-    robot.rotate_power_degrees_IMU(20, -90)
-    distancemeasured = robot.get_ultra_sensor() #reading ultrasonic to see if there is a wall infront
-    if distancemeasured < 20 and distancemeasured != 0:
-        robot.rotate_power_degrees_IMU(20, 180)
-        distancemeasured = robot.get_ultra_sensor()
-        if distancemeasured < 20 and distancemeasured != 0:
-            robot.rotate_power_degrees_IMU(20, 90)
-            movetojunction()
-            #reversed
-        else:
-            movetojunction()
-            #went right
-    else:
-        movetojunction()
-        #turned left
-    return jsonify({ "message":"navigating path obstruction"})
+    return render_template('map.html')
 
 #creates a route to get all the event data
 @app.route('/getallusers', methods=['GET','POST'])
@@ -210,45 +65,10 @@ def getallusers():
     results = database.ViewQueryHelper("SELECT * FROM users")
     return jsonify([dict(row) for row in results]) #jsonify doesnt work with an SQLite.Row
 
-#Get the current command from brickpiinterface.py
-@app.route('/getcurrentcommand', methods=['GET','POST'])
-def getcurrentcommand():
-    return jsonify({"currentcommand":robot.CurrentCommand})
-
-#get the current routine from robot.py
-@app.route('/getcurrentroutine', methods=['GET','POST'])
-def getcurrentroutine():
-    return jsonify({"currentroutine":robot.CurrentRoutine})
-
-#get the configuration status from brickpiinterface
-@app.route('/getconfigured', methods=['GET','POST'])
-def getconfigured():
-    return jsonify({"configured":robot.Configured})
-
-#Start callibration of the IMU sensor
-@app.route('/getcalibration', methods=['GET','POST'])
-def getcalibration():
-    calibration = "Not Calibrated"
-    if robot.calibrate_imu():
-        calibration = "Calibrated"
-    return jsonify({"calibration":calibration})
-
-#Start callibration of the IMU sensor
-@app.route('/reconfigIMU', methods=['GET','POST'])
-def reconfigIMU():
-    robot.reconfig_IMU()
-    return jsonify({"reconfigure":"reconfiguring_IMU"})
-
-#Stop current process
-@app.route('/stop', methods=['GET','POST'])
-def stop():
-    robot.stop_all()
-    return jsonify({ "message":"stopping" })
-
 #Shutdown the web server
 @app.route('/shutdown', methods=['GET','POST'])
 def shutdown():
-    robot.safe_exit()
+    log("shutting down")
     func = request.environ.get('werkzeug.server.shutdown')
     func()
     return jsonify({ "message":"shutting down" })
